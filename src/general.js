1bdd6c2316991618d1b9ff5258e35d544a7fa3b5
WilliamAnimate <75321465+WilliamAnimate@users.noreply.github.com>
2023-12-07 23:58:43 -0500

    put mainModule.js and general.js into main.js

--- src/general.js
+++
@@ -1,8 +0,0 @@
-let develop = false;
-
-// run this file with `async` boolean attribute
-// this file contains code that should be used everywhere here.
-window.addEventListener('contextmenu', e => {
-	e.preventDefault();
-});
-window.addEventListener('keydown', e=>{if(!develop)e.preventDefault()});
--- src/main.js
+++ src/main.js
@@ -1,8 +1,112 @@
+const invoke = window.__TAURI__.invoke
 const btn_advanced = document.getElementById("btn-advanced");
 
-function pollDevelopmentMode() {
-	return develop;
+let lastError; // this variable exists because im too lazy to make a function return promises.
+
+let develop = false;
+
+window.addEventListener('contextmenu', e => {
+	e.preventDefault();
+});
+window.addEventListener('keydown', e=>{if(!develop)e.preventDefault()});
+
+/**
+ * call this function with `await`, then get the value of lastError.
+ * @returns does not. read the value of lastError instead.
+ */
+async function getLastError() {
+	try {
+		lastError = await invoke("get_last_error");
+	} catch (e) {
+		panic("i quit my job, the error handler can't even get an error", "WTF DID YOU DO??? " + e);
+	}
 }
+
+function checkForNewVersion() {
+	const xhr = new XMLHttpRequest();
+	xhr.open("GET", "https://api.github.com/repos/williamanimate/optimize-my-roblos/releases");
+	xhr.setRequestHeader("User-Agent", "optimize_my_roblos"); // FIXME: lmaoooo don't do this
+
+	xhr.onload = async function() {
+		if (xhr.status !== 200) {
+			hideElementById(document.getElementById("cursor-wait-hbox"))
+		}
+
+		const releases = JSON.parse(xhr.responseText);
+		const latest = releases[0].tag_name.slice(1);
+
+		const result = await invoke("get_version");
+		if (!/[^0-9]/.test(latest) || !/[^0-9]/.test(result)) {
+			// assume that a version is like 1.0.0*-alpha* or something similar; don't try
+			hideElementById(document.getElementById("cursor-wait-hbox"));
+			return;
+		}
+		if (latest > result) {
+			const update_text_container = document.getElementById("update_text_container");
+			const update_bypass = document.getElementById("update_bypass");
+
+			hideElementById(update_text_container);
+			hideElementById(update_bypass);
+
+			setTimeout(function() {
+				update_text_container.textContent = `A new version is available: ${latest}`;
+
+				update_bypass.innerHTML = "<p>ok</p>";
+
+				showElementById(update_text_container);
+				showElementById(update_bypass);
+			}, 500);
+		} else {
+			hideElementById(document.getElementById("cursor-wait-hbox"));
+		}
+	}
+
+	xhr.send();
+}
+
+async function tweak(element, funct) {
+	putLoadingAnimationOnId(element);
+	try {
+		var result = await invoke(funct); // use var to keep it declared for the rest of the function
+		// FINALLY a use case for var.
+	} catch (e) {
+		panic("Failed to call the optimize function, does it exist?", e + ". Result info: " + result);
+	}
+	if (result !== true) {
+		await getLastError();
+		panic("Backend threw an error", lastError);
+	}
+	removeLoadingAnimationOnId(element);
+}
+
+const optimizeBtn = document.getElementById("btn-optimize");
+const unoptimizeBtn = document.getElementById("btn-unoptimize");
+const nineteenSeventyFiveTweaksBtn = document.getElementById("btn-adv-1975");
+const officeTweaksBtn = document.getElementById("btn-adv-office");
+const perfTweaksBtn = document.getElementById("btn-adv-perf");
+const optimizeGpuBtn = document.getElementById("btn-adv-optimizegpu");
+
+optimizeBtn.addEventListener("click", async function() {
+	await tweak(optimizeBtn, "optimize_lowspec"); // assume the user is on a slow machine, why else would they be here?
+	showElementById(document.getElementById("done-txt"));
+});
+
+unoptimizeBtn.addEventListener("click", function() {
+	tweak(unoptimizeBtn, "unoptimize");
+});
+
+nineteenSeventyFiveTweaksBtn.addEventListener("click", function() {
+	tweak(nineteenSeventyFiveTweaksBtn, "optimize_1975");
+});
+officeTweaksBtn.addEventListener("click", function() {
+	tweak(officeTweaksBtn, "optimize_office");
+});
+perfTweaksBtn.addEventListener("click", function() {
+	tweak(perfTweaksBtn, "optimize_perf");
+});
+optimizeGpuBtn.addEventListener("click", function() {
+	tweak(optimizeGpuBtn, "optimize_gpu_settings");
+});
 
 function panic(title, message) {
 	document.querySelectorAll('dialog').forEach(element => {
@@ -88,3 +192,15 @@ if (!develop || window.matchMedia('prefers-reduced-motion: reduce')) {
 btn_advanced.addEventListener("click", function() {
 	openDialogById(document.getElementById("_d_advanced"));
 });
+
+invoke("get_version").then((ver) => {
+	document.getElementById("version").textContent = "v" + ver;
+}).catch((err) => {
+	panic("[mainModule]: Failed to fetch version. This could indicate at a severe problem in the Rust backend.", err);
+});
+
+if (!develop) {
+	setTimeout(checkForNewVersion(), 1000);
+} else {
+	setTimeout(hideElementById(document.getElementById("cursor-wait-hbox")), 100);
+}
